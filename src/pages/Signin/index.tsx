196feada34c008beaa83521401039e8bce5003b8
SMarcosM <sebastiaomarcos49@gmail.com>
2022-07-31 17:22:49 -0300

    Phosphor installation, firebase installation and configuration.

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,6 +1,19 @@
+import './styles.scss';
+import {GoogleLogo} from 'phosphor-react'
 
 export function SignIn() {
   return (
-    <h1>Signin</h1>
+    <div className="container">
+      <h1>Acesse sua conta</h1>
+      <span>
+        Utilizando autenticação social, por exemplo, autenticação com a Google você <br/>
+        facilita a vida do usuário permitindo utilizar sua aplicação sem fazer cadastro.
+      </span>
+
+      <button className="button">
+        <GoogleLogo />
+        Entrar com Google
+      </button>
+    </div>
   )
 }
---
+++ src/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8aj7ivfmprklwYFFzgtZC9H9I_WN-X2k",
+  authDomain: "auth-test-62f3e.firebaseapp.com",
+  projectId: "auth-test-62f3e",
+  storageBucket: "auth-test-62f3e.appspot.com",
+  messagingSenderId: "1029922292870",
+  appId: "1:1029922292870:web:0ead91465f542172dc3d07"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
