57779d29b1acae49bcdd32b147714da22d807f6f
SMarcosM <sebastiaomarcos49@gmail.com>
2022-08-01 13:58:07 -0300

    Finished project

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,16 +1,41 @@
+import { useState } from 'react';
+import { GoogleLogo } from 'phosphor-react'
+
+import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth';
+import { auth } from '../../services/firebase'
+
 import './styles.scss';
-import {GoogleLogo} from 'phosphor-react'
+
 
 export function SignIn() {
+  const [user, setUser] = useState<User>({} as User);
+
+  function handleGoogleSignIn() {
+  const provider = new GoogleAuthProvider();
+
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      setUser(result.user);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
   return (
     <div className="container">
+      <div className="user">
+        { user.photoURL && <img src={user.photoURL} alt="Foto do usuário"/>}
+        <strong>{user.displayName}</strong>
+        <small>{user.email}</small>
+      </div>
+
       <h1>Acesse sua conta</h1>
       <span>
         Utilizando autenticação social, por exemplo, autenticação com a Google você <br/>
         facilita a vida do usuário permitindo utilizar sua aplicação sem fazer cadastro.
       </span>
 
-      <button className="button">
+      <button className="button" onClick={handleGoogleSignIn}>
         <GoogleLogo />
         Entrar com Google
       </button>
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getAuth, } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA8aj7ivfmprklwYFFzgtZC9H9I_WN-X2k",
-  authDomain: "auth-test-62f3e.firebaseapp.com",
-  projectId: "auth-test-62f3e",
-  storageBucket: "auth-test-62f3e.appspot.com",
-  messagingSenderId: "1029922292870",
-  appId: "1:1029922292870:web:0ead91465f542172dc3d07"
+  apiKey: 'AIzaSyA8aj7ivfmprklwYFFzgtZC9H9I_WN-X2k',
+  authDomain: 'auth-test-62f3e.firebaseapp.com',
+  projectId: 'auth-test-62f3e',
+  storageBucket: 'auth-test-62f3e.appspot.com',
+  messagingSenderId: '1029922292870',
+  appId: '1:1029922292870:web:0ead91465f542172dc3d07'
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
